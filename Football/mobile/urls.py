a3ce5480ca74a1b5ee6212d6cf2890f804fcaa29
darkedmeow <42093941+darkedmeow@users.noreply.github.com>
2021-03-19 22:11:33 +0300

    Delete Football directory

--- Football/mobile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import include, url
-from . import views
-from django.views.decorators.csrf import csrf_exempt
-
-urlpatterns = [
-    path('start/', views.Start.as_view()),
-    #path('autorization/', csrf_exempt(views.some_view.as_view())),
-]
